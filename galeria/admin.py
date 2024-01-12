341256c5b60349861c15379390abc67c9ee50412
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-01-12 04:40:11 -0300

    Funcionalidades de Publicação

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoNomes(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", "publicada")
+    list_editable = ("publicada",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoNomes)
