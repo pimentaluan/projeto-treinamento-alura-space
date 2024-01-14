64f6b4fc04204376d29423645fa5d6b58d7d2665
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-01-14 20:40:11 -0300

    Opção de fazer upload de fotos e carregar de forma estática na aplicação através do admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,7 +3,7 @@ from galeria.models import Fotografia
 
 
 class ListandoNomes(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "publicada")
+    list_display = ("id", "nome", "legenda", "publicada", "data_fotografia")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria", "publicada")
