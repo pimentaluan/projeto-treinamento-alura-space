b477c55df05fb60380b30bfab7aabd83601d2d2f
Luan Pimenta Fernandes <pimentaluan2005@gmail.com>
2024-01-14 21:26:06 -0300

    Image not found, para quando não houver imagens disponíveis

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
