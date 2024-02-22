4df92c547fb53338a3c46fec253bcb71fc5ce6d8
Luan Pimenta <luan.pimenta@academico.ifpb.edu.br>
2024-02-22 12:01:48 -0300

    Novas, editar e deletar fotografias

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from galeria.models import Fotografia
+from apps.galeria.models import Fotografia
 from django.contrib import messages
 
 def index(request):
@@ -28,3 +28,12 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains = nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+def nova_imagem(request):
+    return render(request, 'galeria/nova_imagem.html')
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
